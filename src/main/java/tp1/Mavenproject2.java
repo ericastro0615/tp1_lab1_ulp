7dac3d717591f62fccae92d29db628d507207004
ericastro0615@gmail.com <ericastro0615@gmail.com>
2023-08-29 17:33:47 -0300

    falta try-catch

---
+++ src/main/java/tp1/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tp1;
+
+/**
+ *
+ * @author Erica
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+    
+    }
+}
